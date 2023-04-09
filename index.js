c4426317d9be9fa5c5f75d435bf0aea2aef555cd
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-04-09 11:36:17 +0100

    Reset and Equals added to JS and HTML

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let firstVar = 0;
 let secondVar = 0;
 let operatorVar = "+";
 let operatorSelected = false; 
-let resetSelected = false;
+let equalsSelected = false;
 let outputNumber = 0; 
 
 function numberPressed(num){
@@ -12,27 +12,52 @@ function numberPressed(num){
         } else{
             secondVar = num;
         }
-        outputNumber = secondVar
+        outputNumber = secondVar;
     } else {
         if (firstVar > 0){
-            firstVar = String(firstVar) + num
+            firstVar = String(firstVar) + num;
         } else{
             firstVar = num;
         }
-        outputNumber = firstVar
+        outputNumber = firstVar;
     }
     document.getElementById("calcOutput").innerHTML = outputNumber;
 }
 
 function deletePressed(){
     if ((operatorSelected) && (outputNumber != 0)) {
-        secondVar = String(secondVar)
+        secondVar = String(secondVar);
         secondVar.length > 1 ? secondVar = secondVar.substring(0, secondVar.length-1) : secondVar = 0; 
-        outputNumber = secondVar
+        outputNumber = secondVar;
     } else {
-        firstVar = String(firstVar)
+        firstVar = String(firstVar);
         firstVar.length > 1 ? firstVar = firstVar.substring(0, firstVar.length-1) : firstVar = 0;
-        outputNumber = firstVar 
+        outputNumber = firstVar; 
     }
+    document.getElementById("calcOutput").innerHTML = outputNumber;
+}
+
+function operatorPressed(op){
+    operatorSelected = true;
+    operatorVar = op; 
+    outputNumber = secondVar;
+    document.getElementById("calcOutput").innerHTML = outputNumber;
+}
+
+function equalsPressed(){ 
+    equalsSelected = true;
+    if ((firstVar > 0) && (secondVar > 0))
+        if(operatorVar == "+"){
+            outputNumber = Number(firstVar) + Number(secondVar)
+        }
+    document.getElementById("calcOutput").innerHTML = outputNumber;
+}
+
+function resetPressed(){ 
+    firstVar = 0; 
+    secondVar = 0; 
+    operatorSelected = false;
+    equalsSelected = true;
+    outputNumber = firstVar; 
     document.getElementById("calcOutput").innerHTML = outputNumber;
 }
