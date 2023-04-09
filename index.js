6d947f7709ed5cfe48f2db1e8bb80fb458046a6f
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-04-09 10:11:06 +0100

    Multiple updates to include initial JS script and button styling.

---
+++ index.js
@@ -0,0 +1,24 @@
+let firstVar = 0;
+let secondVar = 0;
+let operatorSelected = false; 
+let resetSelected = false;
+let outputNumber = 0; 
+
+function numberPressed(num){
+    if(operatorSelected){
+        if (secondVar > 0) {
+            secondVar = str(secondVar) + num;
+        } else{
+            secondVar = num;
+        }
+        outputNumber = secondVar
+    } else {
+        if (firstvar > 0){
+            firstVar = str(secondVar) + num
+        } else{
+            firstVar = num;
+        }
+        outputNumber = firstVar
+    }
+    document.getElementById("calcOutput").innerHTML = outputNumber;
+}
